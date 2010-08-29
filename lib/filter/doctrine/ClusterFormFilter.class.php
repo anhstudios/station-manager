3c1ee571d9df3de7c5cdfae3ddc6456446b925f9
apathy <apathy@swganh.org>
2010-08-29 02:40:11 -0400

    Initial commit

---
+++ lib/filter/doctrine/ClusterFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Cluster filter form.
+ *
+ * @package    station_manager
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ClusterFormFilter extends BaseClusterFormFilter
+{
+  public function configure()
+  {
+  }
+}
