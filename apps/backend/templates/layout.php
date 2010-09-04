6405a2e021660fb39511c383fb7dd58ee8d61f44
apathy <apathy@swganh.org>
2010-09-04 08:52:23 -0400

    Added a basic page layout/design.

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -9,6 +9,26 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+    <div id="main">
+      <div id="header">
+        <div id="logo" class="site-section">
+          <h1><a href="<?php echo url_for('@homepage'); ?>">Station</a></h1>
+        </div>
+      </div>
+      
+      <div id="page">
+        <div id="content" class="site-section">
+          <?php echo $sf_content ?>
+        </div>
+      </div>
+    </div>  
+      
+    <div id="footer">
+      <div id="footer-content">
+        <div id="copyright-notice" class="site-section">
+          Copyright &copy; 2010 - ANH Studios
+        </div>
+      </div>
+    </div>
   </body>
 </html>
